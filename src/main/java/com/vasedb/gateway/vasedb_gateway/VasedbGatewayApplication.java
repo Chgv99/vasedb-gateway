29615de781d242e661e7ee435e406b2c2b766b00
Christian G <christiangv99@gmail.com>
2025-06-08 19:17:57 +0100

    First commit

---
+++ src/main/java/com/vasedb/gateway/vasedb_gateway/VasedbGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.vasedb.gateway.vasedb_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VasedbGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VasedbGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vasedb/gateway/vasedb_gateway/VasedbGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasedb.gateway.vasedb_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VasedbGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
